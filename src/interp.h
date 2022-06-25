d83fa08dbfece926f3b45b3d7927f9c8aea3a6fa
Daniel Anderson <dan.w.anderson@gmail.com>
2022-06-25 17:16:32 -0400

    update copyright

--- src/interp.h
+++ src/interp.h
@@ -1,5 +1,5 @@
 ///////////////////////////////////////////////////////////////////////
-////  Broken Shadows (c) 1995-1999 by Daniel Anderson
+////  Broken Shadows (c) 1995-2022 by Daniel Anderson
 ////  
 ////  Permission to use this code is given under the conditions set
 ////  forth in ../doc/shadows.license
