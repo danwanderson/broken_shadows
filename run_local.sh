ba0ce11499a9edd2781efb547638432379a7618d
Daniel Anderson <dan.w.anderson@gmail.com>
2022-06-25 16:35:07 -0400

    update to add volumes

--- run_local.sh
+++ run_local.sh
@@ -3,10 +3,17 @@
 set -euo pipefail
 # set -x
 
-docker run --rm -it --publish 4000:4000 --entrypoint /bin/bash danwanderson/broken_shadows:latest
+docker run --rm -it --publish 4000:4000 \
+  --volume ${PWD}/area:/srv/shadows/area \
+  --volume ${PWD}/gods:/srv/shadows/gods \
+  --volume ${PWD}/log:/srv/shadows/log \
+  --volume ${PWD}/notes:/srv/shadows/notes \
+  --volume ${PWD}/player:/srv/shadows/player \
+  --volume ${PWD}/core:/srv/shadows/core \
+  --entrypoint /bin/bash danwanderson/broken_shadows:latest
 
-sleep 10s
+#sleep 10
 
-IPADDR=$(ip addr show | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | awk -F '/' '{print $1}')
+#IPADDR=$(ip addr show | grep "inet " | grep -v "127.0.0.1" | awk '{print $2}' | awk -F '/' '{print $1}')
 
-telnet "${IPADDR}" 4000
+#telnet "${IPADDR}" 4000
