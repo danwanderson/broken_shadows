b038ea02cd0d8a3de029c4da1e0f50aedbb5aff3
Daniel Anderson <dan.w.anderson@gmail.com>
2024-08-04 13:30:09 -0400

    remove buildx create

--- build_image.sh
+++ build_image.sh
@@ -3,7 +3,7 @@
 set -euo pipefail
 # set -x
 
-docker buildx create --driver docker-container --use
+# docker buildx create --driver docker-container --use
 
 docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag danwanderson/broken_shadows:latest --tag danwanderson/broken_shadows:$(date +"%Y%m%d") .
 
