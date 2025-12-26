50e0fdea2b286cdf67ead92ca6bb487686689fc9
Daniel Anderson <dan.w.anderson@gmail.com>
2025-12-26 15:29:56 -0500

    update to reflect newer docker syntax

--- build_image.sh
+++ build_image.sh
@@ -4,6 +4,7 @@ set -euo pipefail
 # set -x
 
 # docker buildx create --driver docker-container --use
+DATE=$(date +"%Y%m%d")
 
-docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag danwanderson/broken_shadows:latest --tag danwanderson/broken_shadows:$(date +"%Y%m%d") .
+docker buildx build --push --platform linux/arm/v7,linux/arm64/v8,linux/amd64 --tag danwanderson/broken_shadows:latest --tag danwanderson/broken_shadows:"${DATE}" --tag registry.dwa.dev:443/broken_shadows:latest --tag registry.dwa.dev:443/broken_shadows:"${DATE}" .
 
--- update_image.sh
+++ update_image.sh
@@ -4,7 +4,7 @@ DATE=$(date +%Y%m%d_%H%M)
 
 
 tar cvf ~/shadows."${DATE}".tar *
-docker-compose down
-docker pull danwanderson/broken_shadows:latest
-docker-compose up -d
+docker compose down
+docker compose pull
+docker compose up -d
 docker image prune --force
